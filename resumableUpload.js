b3484f499ae512f45f2a765b4038e4f67bac09e5
kamneklogs <kamneklogs@gmail.com>
2022-03-06 21:38:14 -0500

    Resumable approach completed

--- resumableUpload.js
+++ resumableUpload.js
@@ -1,33 +1,62 @@
-var axios = require('axios');
+var axios = require("axios");
+var fs = require("fs");
 
-try { // Load credentials
-    var config = require('./config.json')
+try {
+  // Load credentials
+  var config = require("./config.json");
 } catch (error) {
-    console.error('Credentials not found')
+  console.error("Credentials not found");
 }
+
+config["Upload-Offset"];
+var path = config.filePath;
+var file = fs.createReadStream(path);
+var size = fs.statSync(path).size;
 
 const headers = {
-    'Authorization': 'bearer ' + config.access_token,
-    'Content-Type': 'application/json',
-    'Acept': 'application/vnd.vimeo.*+json;version=3.4'
-}
+  Authorization: "bearer " + config.access_token,
+  "Content-Type": "application/json",
+  Acept: "application/vnd.vimeo.*+json;version=3.4",
+};
 
 const data = {
-    "upload": {
-        "approach": "tus",
-        "size": "1055736"
-    }
-}
+  upload: {
+    approach: "tus",
+    size: size,
+  },
+};
 
-axios.post('https://api.vimeo.com/me/videos', data, {
-    headers: headers
-})
-    .then(upload(response))
-    .catch((error) => {
-        console.error(error)
+axios
+  .post("https://api.vimeo.com/me/videos", data, {
+    headers: headers,
+  })
+  .then((response) => {
+    upload(response.data.upload.upload_link, 0);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+function upload(upload_link, point) {
+  axios
+    .patch(upload_link, file, {
+      headers: {
+        "Tus-Resumable": "1.0.0",
+        "Upload-Offset": point,
+        "Content-Type": "application/offset+octet-stream",
+        Acept: "application/vnd.vimeo.*+json;version=3.4",
+      },
     })
+    .then((response) => {
+      var partialUpload = Number(response.headers["upload-offset"]);
 
-function upload(response) {
-    console.log(response)
-    // To upload thye video file
+      if (partialUpload != size) {
+        upload(upload_link, partialUpload);
+      } else {
+        console.info("Upload success");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
